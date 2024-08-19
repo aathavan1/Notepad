c76cb9ee301f42ed535c0ea5bd5137983fd1ca61
Aathavan G <aathavan972004@gmail.com>
2024-08-19 12:48:21 +0530

    dvd

--- server/index.js
+++ server/index.js
@@ -6,11 +6,11 @@ const app = express()
 
 app.use(cors(
 
-    // {
-        // origin: ["https://bike-service-frontend-six.vercel.app/"],
+    {
+        origin: "https://notepad-client-two.vercel.app"
         // methods: ["POST", "GET","PUT","DELETE"],
         // credentials: true
-    // }
+    }
 ))
 app.use(express.json())
 
