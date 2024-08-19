509eb7d5fb44bd1afc67835ebb5c95c198fe7765
Aathavan G <aathavan972004@gmail.com>
2024-08-19 12:18:37 +0530

    s

--- server/index.js
+++ server/index.js
@@ -4,14 +4,14 @@ const dataModel = require('./models/dataModel')
 
 const app = express()
 
-app.use(cors(
-    {
-        origin: "https://notepad-client-two.vercel.app"
-        // methods: ["POST", "GET","PUT","DELETE"]
-        // credentials: true
+// app.use(cors(
+//     {
+//         origin: "https://notepad-client-two.vercel.app"
+//         // methods: ["POST", "GET","PUT","DELETE"]
+//         // credentials: true
 
-    }
-))
+//     }
+// ))
 app.use(express.json())
 
 app.use('/user', require('./routes/user'))
