3919bab6a2b11210a666ab134e719fe88949114c
Aathavan G <aathavan972004@gmail.com>
2024-08-19 12:29:41 +0530

    y

--- server/index.js
+++ server/index.js
@@ -5,11 +5,11 @@ const dataModel = require('./models/dataModel')
 const app = express()
 
 app.use(cors(
-    // {
-    //     origin: "https://notepad-client-two.vercel.app"
-    //     // methods: ["POST", "GET","PUT","DELETE"]
-    //     // credentials: true
 
+    // {
+        // origin: ["https://bike-service-frontend-six.vercel.app/"],
+        // methods: ["POST", "GET","PUT","DELETE"],
+        // credentials: true
     // }
 ))
 app.use(express.json())
